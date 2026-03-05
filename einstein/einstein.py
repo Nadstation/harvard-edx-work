c7ee4edbb47719b2dfb89dcf09cb3a097738a9d9
Nadstation <nad.benha@gmail.com>
2026-03-05 12:10:39 +0100

    Update einstein.py

--- einstein/einstein.py
+++ einstein/einstein.py
@@ -1,9 +1,4 @@
-"""
-Even if you haven’t studied physics (recently or ever!), you might have heard that 𝐸 =𝑚⁢𝑐2,
-wherein 𝐸 represents energy (measured in Joules), 𝑚 represents mass (measured in kilograms),
-and 𝑐 represents the speed of light (measured approximately as 300000000 meters per second), per Albert Einstein et al.
-Essentially, the formula means that mass and energy are equivalent.
-"""
+#https://cs50.harvard.edu/python/psets/0/einstein/
 
 c = 300000000
 
